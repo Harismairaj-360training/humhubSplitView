a5fe3595c6aab66962f2ebd881c8314adc281860
Haris Mairaj <haris.mairaj@360training.com>
2017-12-24 20:27:58 +0500

    Update message and remove adsense mobile height

---
+++ protected/config/messages/en-US/UserModule.views_auth_login.php
@@ -0,0 +1,4 @@
+<?php
+return array (
+  "Don't have an account? Join the network by entering your e-mail address." => "Don't have an account? Join the network by entering your e-mail address.<br><br> <strong>Note:</strong> Please read our privacy policy, posting policy and disclaimer before registering yourself. When you register yourself (via email), you agree that you have read all our policies and disclaimer."
+);
